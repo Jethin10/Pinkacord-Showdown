3808f534124ea369bdd8fd277cf86b2d52d91f2b
Jethin10 <jethin047@gmail.com>
2026-07-04 21:40:42 +0530

    Pinkacord content update by Volc is GOD: admin panel update
    added: config/custom-formats.ts
    modified: content/pinkacord/items.json
    modified: data/mods/pinkacord/items.ts
    
    Published via Pinkacord admin panel

---
+++ config/custom-formats.ts
@@ -0,0 +1,15 @@
+// THIS FILE IS GENERATED FROM content/. DO NOT EDIT BY HAND.
+// Edit the corresponding file in content/ and run `npm run pinkacord:build`.
+// Generator: tools/pinkacord/generator.ts
+
+export const Formats: import('../sim/dex-formats').FormatList = [
+	{ section: "Pinkacord", column: 1 },
+	{
+		name: "[Pinkacord] Lowering powercreep",
+		desc: "Banning mons",
+		mod: "gen9",
+		gameType: "singles",
+		ruleset: ["Standard", "Evasion Clause"],
+		banlist: ["Uber", "AG", "Moody", "Shadow Tag", "Arena Trap", "King's Rock", "Razor Fang", "Baton Pass", "Last Respects", "Shed Tail", "Tera Blast", "Ogerpon-Wellspring", "Kyurem", "Ceruledge"],
+	},
+];
--- data/mods/pinkacord/items.ts
+++ data/mods/pinkacord/items.ts
@@ -3,4 +3,18 @@
 // Generator: tools/pinkacord/generator.ts
 
 export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
+	testingobject: {
+		num: 10300,
+		name: "testing object",
+		shortDesc: "A custom held item.f",
+	
+		onResidualOrder: 5,
+		onResidualSubOrder: 4,
+		onResidual(pokemon) {
+			if (pokemon.hp < pokemon.maxhp) {
+				this.heal(pokemon.baseMaxhp / 4, pokemon);
+			}
+		},
+		gen: 9,
+	},
 };
