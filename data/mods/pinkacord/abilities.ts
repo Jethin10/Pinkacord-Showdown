24e255a54658c01006268761fc95c1c08eaad8e9
Jethin10 <jethin047@gmail.com>
2026-07-05 16:44:29 +0530

    Pinkacord content update by Unknown: admin panel update
    modified: content/pinkacord/abilities.json
    modified: data/mods/pinkacord/abilities.ts
    
    Published via Pinkacord admin panel

--- data/mods/pinkacord/abilities.ts
+++ data/mods/pinkacord/abilities.ts
@@ -3,4 +3,11 @@
 // Generator: tools/pinkacord/generator.ts
 
 export const Abilities: import('../../../sim/dex-abilities').ModdedAbilityDataTable = {
+	arcticafterglow: {
+		name: "Arctic Afterglow",
+		shortDesc: "Summon Aurora Veil on entry",
+
+		flags: {},
+		gen: 9,
+	},
 };
